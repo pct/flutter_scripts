73234724b78d48fc5384d7a9cd5cb5f4cdeb29ed
Jin-Sih Lin <linpct@gmail.com>
2020-06-06 08:27:46 +0800

    feat: add flutter_install.sh

---
+++ flutter_install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# 安裝 cli 版的 app store
+brew install mas
+
+# 裝 Xcode
+mas install 497799835
+
+# asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+echo "source ~/.asdf/asdf.fish" >> ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+asdf update
+
+# flutter dev
+brew install dart
+asdf plugin-add flutter
+
+asdf install flutter 1.17.2-stable
+
+# remove asdf
+#rm -rf ~/.tool-versions ~/.asdf
+
